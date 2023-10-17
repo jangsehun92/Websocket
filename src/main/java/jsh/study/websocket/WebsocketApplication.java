0a9740b9fcb9e33cea12b412ba91826947d6025e
jangsehun92 <jangsehun92@gmail.com>
2023-10-17 21:39:30 +0900

    first commit

---
+++ src/main/java/jsh/study/websocket/WebsocketApplication.java
@@ -0,0 +1,13 @@
+package jsh.study.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsh/study/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.study.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
