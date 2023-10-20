6225a1619dd302df8e59cf711cf6f7733c03740d
jangsehun92 <jangsehun92@gmail.com>
2023-10-20 19:26:55 +0900

    Add: WebsocketConfig

---
+++ src/main/java/jsh/study/websocket/config/WebsocketConfig.java
@@ -0,0 +1,21 @@
+package jsh.study.websocket.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebsocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/connect").setAllowedOrigins("*");
+    }
+}
