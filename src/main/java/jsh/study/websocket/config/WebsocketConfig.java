3fd7a8fe26fcd548eb01c0e0aea96519ab498515
jangsehun92 <jangsehun92@gmail.com>
2023-11-13 20:34:00 +0900

    Modify: stomp

--- src/main/java/jsh/study/websocket/config/WebsocketConfig.java
+++ src/main/java/jsh/study/websocket/config/WebsocketConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -15,12 +16,21 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*"); //.withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/queue", "/topic");
+        // heartbeat 설정
+        ThreadPoolTaskScheduler ts = new ThreadPoolTaskScheduler();
+        ts.setPoolSize(1);
+        ts.setThreadNamePrefix("ws-heartbeat-thread-");
+        ts.initialize();
+
+        registry.enableSimpleBroker("/queue", "/topic")
+                .setHeartbeatValue(new long[]{5000,5000})
+                .setTaskScheduler(ts);
+
         registry.setApplicationDestinationPrefixes("/app");
     }
 
